c14069997c1b623e1147302657aff0a3ece14bd3
juro-nkw02 <nyeinkyawwin11@gmail.com>
2023-10-14 05:28:28 +0630

    first commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import laravel from "laravel-vite-plugin";
+
+export default defineConfig({
+    plugins: [
+        react(),
+        laravel({
+            input: "resources/js/app.jsx",
+            refresh: true,
+        }),
+    ],
+    server: {
+        host: true,
+        port: 3000,
+        hmr: {
+            host: "localhost",
+        },
+    },
+});
